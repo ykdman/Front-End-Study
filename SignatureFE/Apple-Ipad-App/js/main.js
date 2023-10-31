6df29c3c87d2566f892ee3b41d8d1e7f2caf5a2b
YKDman <dbsrudejr300@gmail.com>
2023-10-31 22:00:47 +0900

    장바구니 메뉴 완성

---
+++ SignatureFE/Apple-Ipad-App/js/main.js
@@ -0,0 +1,31 @@
+// 장바구니 메뉴 띄우기 / 지우기
+const basketStarterEL = document.querySelector('header .basket-starter');
+const baksetEl = basketStarterEL.querySelector('.basket');
+
+basketStarterEL.addEventListener('click', function (event) {
+  event.stopPropagation(); //상위 객체로의 전파를 멈춘다.
+  if (baksetEl.classList.contains('show')) {
+    //hide
+    hideBasket();
+  } else {
+    //show
+    showBasket();
+  }
+});
+
+baksetEl.addEventListener('click', function (event) {
+  // baksetStarterEl > basketEl 이기 떄문에, basketEl 안에서의 클릭이 상위로의 전파를 방지해야 장바구니가 열린다.
+  event.stopPropagation();
+});
+
+window.addEventListener('click', function () {
+  hideBasket();
+});
+
+function showBasket() {
+  baksetEl.classList.add('show');
+}
+
+function hideBasket() {
+  baksetEl.classList.remove('show');
+}
